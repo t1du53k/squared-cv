77ce3d3c2ff4b103e0a9e464c51ae83f96d3fba9
rawram <rawramgaimz@gmail.com>
2014-05-16 17:00:36 +0200

    Dodanie kilku rzeczy

--- js/fillIcon.js
+++ js/fillIcon.js
@@ -1,61 +1,41 @@
-var squaredIcon = $("#squaredicon");
-for (var row = 0; row < 20; row += 1) {
-    for (var column = 0; column < 20; column += 1) {
-        var filling = lightBulb[row][column];
-        if (filling == 0) {
-            if (row == 0) {
-                if (column == 0) {
-                    emptySquare = $("<div class='empty top left miniblock'></div>");
-                    squaredIcon.append(emptySquare);
-                } else if (column == 19) {
-                    emptySquare = $("<div class='empty top right miniblock'></div>");
-                    squaredIcon.append(emptySquare);
-                } else {
-                    emptySquare = $("<div class='empty top miniblock'></div>");
-                    squaredIcon.append(emptySquare);
-                }
-            }   else {
-                if (column == 0) {
-                    emptySquare = $("<div class='empty left miniblock'></div>");
-                    squaredIcon.append(emptySquare);
-                } else if (column == 19) {
-                    emptySquare = $("<div class='empty right miniblock'></div>");
-                    squaredIcon.append(emptySquare);
-                } else {
-                    emptySquare = $("<div class='empty miniblock'></div>");
-                    squaredIcon.append(emptySquare);
-                }
-            }
-        } else if (filling == 2) {
-            filledSquare = $("<div class='semifilled filled miniblock'></div>");
-            squaredIcon.append(filledSquare);
-        } else if (filling == 2) {
-            filledSquare = $("<div class='semifilled filled miniblock'></div>");
-            squaredIcon.append(filledSquare);
-        } else {
-            if (row == 0) {
-                if (column == 0) {
-                    filledSquare = $("<div class='filled top left miniblock'></div>");
-                    squaredIcon.append(filledSquare);
-                } else if (column == 19) {
-                    filledSquare = $("<div class='filled top right miniblock'></div>");
-                    squaredIcon.append(filledSquare);
-                } else {
-                    filledSquare = $("<div class='filled top miniblock'></div>");
-                    squaredIcon.append(filledSquare);
-                }
-            } else {
-                if (column == 0) {
-                    filledSquare = $("<div class='filled left miniblock'></div>");
-                    squaredIcon.append(filledSquare);
-                } else if (column == 19) {
-                    filledSquare = $("<div class='filled right miniblock'></div>");
-                    squaredIcon.append(filledSquare);
-                } else {
-                    filledSquare = $("<div class='filled miniblock'></div>");
-                    squaredIcon.append(filledSquare);
-                }
-            }
-        }
+var squaredIcon = $("#squaredicon"),
+    kupa = $("#kupa");
+
+        
+function appendMiniBlocks() {
+    for (var row = 0; row < 20; row += 1) {
+        squaredRow = $("<div class='squaredRow'>");
+        squaredIcon.append(squaredRow);
+        for (var column = 0; column < 20; column += 1) {
+            var filling = lightBulb2[row][column];
+            switch (filling) {
+            case 0:
+                emptySquare = $("<div class='empty miniblock'></div>");
+                squaredRow.append(emptySquare);
+                break;
+            case 1:
+                emptySquare = $("<div class='filled miniblock'></div>");
+                squaredRow.append(emptySquare);
+                break;
+            case 2:
+                emptySquare = $("<div class='accent miniblock'></div>");
+                squaredRow.append(emptySquare);
+                break;
+            case 3:
+                emptySquare = $("<div class='progress miniblock'></div>");
+                squaredRow.append(emptySquare);
+                break;
+            };
+        };
     };
 };
+function displayProgress(progress) {
+    for (var row = 20; row > 0; row -= 1) {
+        for (var column = 0; column < 20; column += 1) {
+            if (progress >= row) {
+                
+            }
+        }
+    }
+};
+appendMiniBlocks();
