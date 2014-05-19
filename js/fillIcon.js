432b11630f0a969b5dde040f087bf3a447b22fe6
Michał Warda <t1du53k@gmail.com>
2014-05-19 09:58:04 +0200

    Dużo zrobionego wrzucony bootstrap działająca w miarę animacja ale bez zmiennych

--- js/fillIcon.js
+++ js/fillIcon.js
@@ -1,13 +1,10 @@
-var squaredIcon = $("#squaredicon"),
-    kupa = $("#kupa");
-
-        
+var squaredIcon = $("#squaredicon");
 function appendMiniBlocks() {
     for (var row = 0; row < 20; row += 1) {
         squaredRow = $("<div class='squaredRow'>");
         squaredIcon.append(squaredRow);
         for (var column = 0; column < 20; column += 1) {
-            var filling = lightBulb2[row][column];
+            var filling = ruby[row][column];
             switch (filling) {
             case 0:
                 emptySquare = $("<div class='empty miniblock'></div>");
@@ -29,13 +26,62 @@ function appendMiniBlocks() {
         };
     };
 };
-function displayProgress(progress) {
-    for (var row = 20; row > 0; row -= 1) {
-        for (var column = 0; column < 20; column += 1) {
-            if (progress >= row) {
-                
-            }
-        }
-    }
+function attachNumber() {
+    $('.squaredRow').each(function(index, value) {
+        $(this).attr("class", "squaredRow row" + index);
+    });
+};
+function displayProgress() {
+    $("#squaredicon").click(function() {
+        $(".row19").children().toggleClass("shining");
+        $("#progressPercentage").text("5%");
+        $(this).delay(100).queue(function(){
+            $(".row18").children().toggleClass("shining");
+            $("#progressPercentage").text("10%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row17").children().toggleClass("shining");
+            $("#progressPercentage").text("15%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row16").children().toggleClass("shining");
+            $("#progressPercentage").text("20%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row15").children().toggleClass("shining");
+            $("#progressPercentage").text("25%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row14").children().toggleClass("shining");
+            $("#progressPercentage").text("30%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row13").children().toggleClass("shining");
+            $("#progressPercentage").text("35%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row12").children().toggleClass("shining");
+            $("#progressPercentage").text("40%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row11").children().toggleClass("shining");
+            $("#progressPercentage").text("45%");
+            $(this).dequeue();
+        });
+        $(this).delay(100).queue(function(){
+            $(".row10").children().toggleClass("shining");
+            $("#progressPercentage").text("50%");
+            $(this).dequeue();
+        });
+    });
 };
 appendMiniBlocks();
+attachNumber();
+displayProgress();
